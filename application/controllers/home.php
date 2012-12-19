bd02c3b671cd06bab74d57941fcf7cef1b49c175
Daz <yunhen3350@hotmail.com>
2012-12-19 14:00:30 +0800

    wrapper for initial controller

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,7 +2,19 @@
 
 class Home extends CI_Controller {
 
+	public function load_view_wrapper($view = '', $data = '') {
+		$this->load->view('templates/header', $data);
+
+		if($view) {
+			$this->load->view('home_view', $data);
+		}
+		
+		$this->load->view('templates/footer', $data);
+	}
+
 	public function index() {
-		$this->load->view('home_view');
+		$data['title'] = 'Home';
+
+		$this->load_view_wrapper('home_view', $data);
 	}
 }
---
+++ application/views/templates/footer.php
@@ -0,0 +1,22 @@
+            <hr>
+
+            <footer>
+                <p>&copy; Company 2012</p>
+            </footer>
+
+        </div> <!-- /container -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="<?php echo base_url() ?>assets/js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        <script src="<?php echo base_url() ?>assets/js/vendor/bootstrap.min.js"></script>
+        <script src="<?php echo base_url() ?>assets/js/main.js"></script>
+<?php /*
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+*/ ?>
+    </body>
+</html>
